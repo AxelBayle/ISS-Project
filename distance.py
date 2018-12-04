1a981bfb2eb3f53fccc25f601d69040c10e94fd3
Axel.Bayle <axelb@skylights.aero>
2018-12-04 15:26:22 +0100

    fonction distance

--- distance.py
+++ distance.py
@@ -1,9 +1,38 @@
 import pigpio as GPIO
+import time
+PIN_TRIG = 20
+PIN_ECHO = 21
 
-# pin led 21
+def init_distance():
+    pi = GPIO.pi()
+    pi.set_mode( PIN_ECHO, GPIO.INPUT)
+    pi.set_mode( PIN_TRIG, GPIO.OUTPUT)
+    pi.write(PIN_TRIG,False)
+    time.sleep(0.5)
 
-pi=GPIO.pi()
 
-pi.set_mode(21,GPIO.OUTPUT)
-pi.write(21,1)
-print("toto")
+def distance () :
+    pi=GPIO.pi()
+    # pulse sur la pin echo
+    pi.write(PIN_ECHO,True)
+    time.sleep(0.00001)
+    pi.write(PIN_ECHO,False)
+    start =time.time()
+    stop = -1
+    while ( pi.read(PIN_ECHO)==False):
+        start=time.time()
+    while ( pi.read(PIN_ECHO)==True):
+        stop=time.time()
+    if stop == -1 :
+        print ("Erreur")
+        return -1
+    else :
+        delai=stop-start
+        distance= delai*34000/2 # vitesse son / 2 fois distance
+        print("Distance : ",distance)
+        return distance
+
+
+if __name__=='__main__':
+    init_distance()
+    x=distance()
